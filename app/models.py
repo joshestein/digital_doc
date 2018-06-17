0ce5feb97b7cbf282ad64d356ddaf9fa9c63d041
Josh <josh.e.stein@gmail.com>
2018-06-17 09:33:17 +0200

    Working doctor registration

--- app/models.py
+++ app/models.py
@@ -1,10 +1,32 @@
-from app import db
+from app import db, login
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
-class Doctor(db.Model):
+@login.user_loader
+def load_user(id):
+    return Doctor.query.get(int(id))
+
+class Doctor(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64), index = True, unique = True)
     email = db.Column(db.String(128), index= True, unique = True)
     password_hash = db.Column(db.String(128))
+    patients = db.relationship('Patient', backref = 'doctor', lazy = 'dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return '<Doctor {}>'.format(self.username)
+
+class Patient(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(64), index = True)
+    age = db.Column(db.Integer)
+    doc_id = db.Column(db.Integer, db.ForeignKey('doctor.id'))
+
+    def __repr__(self):
+        return 'Patient {}'.format(self.name)
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,14 @@
-from flask import render_template, flash, redirect
-from app import app
-from app.forms import LoginForm
+from flask import render_template, flash, redirect, request, url_for
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from app.models import Doctor
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'Stefan'}
     patients = [
         {
             'name':'Josh',
@@ -16,12 +19,40 @@ def index():
             'age':20
         }
     ]
-    return render_template('index.html', title='Home', user=user, patients=patients)
+    return render_template('index.html', title='Home', patients=patients)
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        doc = Doctor.query.filter_by(username=form.username.data).first()
+        if doc is None or not doc.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(doc, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title = 'Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        doc = Doctor(username = form.username.data, email = form.email.data)
+        doc.set_password(form.password.data)
+        db.session.add(doc)
+        db.session.commit()
+        flash('Congratulations, you have succesfully registered.')
+        return redirect(url_for('login'))
+    return render_template('register.html', title = 'Register', form=form)
