90a88f53efe1a0c91369c615e9840f5829fa6239
Josh <josh.e.stein@gmail.com>
2018-07-24 15:44:11 +0200

    Added fixes

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, SelectField
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, NumberRange
 from app.models import Doctor, Patient
 
@@ -23,9 +23,11 @@ class RegistrationForm(FlaskForm):
             raise ValidationError('Please use a different email address.')
 
 class AddPatientForm(FlaskForm):
-    name = StringField('Patient Name', validators=[DataRequired()])
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
     age = IntegerField('Age', validators = [NumberRange(0, 120, 'Invalid age')])
-    email = StringField('Email')
+    sex = SelectField('Sex', choices=[('m', 'Male'), ('f','Female'), ('o', 'Other')])
+    email = StringField('Email', validators = [DataRequired(), Email()])
     submit = SubmitField('Add')
 
 class RequestPasswordResetForm(FlaskForm):
